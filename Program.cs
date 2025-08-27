837afb32f72b80c42f8149eb40525692875b5042
ttadesse627 <ttadesse627@gmail.com>
2025-08-27 13:13:04 +0300

    Injected dependeny and configuration setting

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
+using Yaya.Webhook.API;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://127.0.0.1:8000"); //used static host to ignore randomly generated Ports on startup
 builder.Services.AddControllers();
+
+builder.Services.AddTransient<IWebhookProcessor, WebhookProcessor>(); //Dependency Injection
+builder.Services.Configure<YayaWebhookSettings>( // Add setting services
+    builder.Configuration.GetSection("YayaWallet"));
+
 var app = builder.Build();
 
 app.MapControllers();
